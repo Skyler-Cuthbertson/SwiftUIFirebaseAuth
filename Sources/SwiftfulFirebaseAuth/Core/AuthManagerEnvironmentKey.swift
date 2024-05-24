79d9ed55b16d6417561bd16e11c3fead87f98dc6
Skyler Cuthbertson <skylercuthbertson2001@gmail.com>
2024-05-23 23:01:13 -0500

    remove env key auth

--- Sources/SwiftfulFirebaseAuth/Core/AuthManagerEnvironmentKey.swift
+++ Sources/SwiftfulFirebaseAuth/Core/AuthManagerEnvironmentKey.swift
@@ -4,18 +4,18 @@
 //
 //  Created by Nick Sarno on 12/10/23.
 //
-
-import Foundation
-import SwiftUI
-
-public struct AuthManagerEnvironmentKey: EnvironmentKey {
-    @MainActor
-    public static let defaultValue: AuthManager = AuthManager(configuration: .mock)
-}
-
-public extension EnvironmentValues {
-    var auth: AuthManager {
-        get { self[AuthManagerEnvironmentKey.self] }
-        set { self[AuthManagerEnvironmentKey.self] = newValue }
-    }
-}
+//
+//import Foundation
+//import SwiftUI
+//
+//public struct AuthManagerEnvironmentKey: EnvironmentKey {
+//    @MainActor
+//    public static let defaultValue: AuthManager = AuthManager(configuration: .mock)
+//}
+//
+//public extension EnvironmentValues {
+//    var auth: AuthManager {
+//        get { self[AuthManagerEnvironmentKey.self] }
+//        set { self[AuthManagerEnvironmentKey.self] = newValue }
+//    }
+//}
